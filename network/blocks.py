Try fix gradient-modified variable

network/blocks.py
@@ -130,7 +130,7 @@ class ResidualBlock(nn.Module):
         else:
             self.bn1 = None
             self.bn2 = None
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
         self.conv2 = nn.utils.spectral_norm(conv3x3(out_channels, out_channels))
         self.downsample = downsample
 
@@ -218,7 +218,7 @@ class ResidualBlockUp(nn.Module):
         else:
             self.bn1 = self.bn2 = self.bn3 = None
 
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
         self.conv2 = nn.utils.spectral_norm(conv3x3(in_channels, out_channels))
         self.conv3 = nn.utils.spectral_norm(conv3x3(out_channels, out_channels))
 

train.py
@@ -174,10 +174,10 @@ def main():
 
                 # train G and D
                 fake = G(img, frames, marks)
-
                 fake_score, d_fake_list = D(fake, mark)
 
-                real_score, d_real_list = D(img, mark)
+                with torch.no_grad():
+                    real_score, d_real_list = D(img, mark)
 
                 loss_generator = loss_g(
                     img, fake, fake_score, d_fake_list, d_real_list
@@ -188,6 +188,7 @@ def main():
             with torch.autograd.enable_grad():
                 optimizerG.zero_grad()
                 optimizerD.zero_grad()
+                fake.detach_().requires_grad_()
                 fake_score, d_fake_list = D(fake, mark)
                 loss_fake = loss_d_fake(fake_score)
 