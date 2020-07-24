8ca958452ccebfe95122a9afcee3be78d97340bd
Nikolay Mahotkin <nikolay.makhotkin@gmail.com>
2020-07-24 10:34:32 +0400

    Fix imports

--- network/model.py
+++ network/model.py
@@ -3,8 +3,8 @@ import math
 import torch
 import torch.nn as nn
 
-from marionette.network import blocks
-from marionette.network import unet
+from network import blocks
+from network import unet
 
 E_LEN = 512
 
